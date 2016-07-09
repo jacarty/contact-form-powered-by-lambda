91fb57d21449317037189cad482859af55dfef65
James Carty <administrator@jamescarty.co.uk>
2016-07-09 15:25:54 +0100

    Removing navbar fix

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,12 +1,3 @@
-// Fix Hide Menu on Anchor Click
-$(".navbar-nav li a").click(function (event) {
-   // check if window is small enough so dropdown is created
-   var toggle = $(".navbar-toggle").is(":visible");
-   if (toggle) {
-     $(".navbar-collapse").collapse('hide');
-   }
-});
-
 // Contact form submission
     (function($){
         function processForm(e){
